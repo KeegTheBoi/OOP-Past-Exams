6fe91782e022cc8a1eba723b8b13a0486aa10aa9
Keegan Carlo Falcao - keegancarlo.falcao@studio.unibo.it <keegancarlo.falcao@studio.unibo.it>
2023-12-10 17:38:25 +0100

    flattener updated with streams

--- 2022/a01b/e1/FlattenerFactoryImpl.java
+++ 2022/a01b/e1/FlattenerFactoryImpl.java
@@ -1,168 +1,47 @@
 package a01b.e1;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.function.Function;
-import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class FlattenerFactoryImpl implements FlattenerFactory {
 
-    private <X, Y> Flattener<X, Y> generalFlatten(Predicate<List<List<X>>> testing, Function<List<List<X>>, List<Y>> mapper){
-        return new Flattener<X,Y>() {
 
-            @Override
-            public List<Y> flatten(List<List<X>> list) {
-                List<Y> listFlatten = new ArrayList<>();
-                List<List<X>> temp = new ArrayList<>();
-                for (List<X> listSIn : list) {
-                    temp.add(listSIn);
-                    if(testing.test(temp)){
-                        listFlatten.addAll(mapper.apply(temp));
-                        temp.clear();
-                    }
-                }
-                if(!temp.isEmpty()){
-                    listFlatten.addAll(mapper.apply(temp));
-                }
-                return listFlatten;
-            }
-            
-        };
-    }
 
     @Override
     public Flattener<Integer, Integer> sumEach() {
-        
-
-        return generalFlatten(new Predicate<List<List<Integer>>>() {
-
-            @Override
-            public boolean test(List<List<Integer>> t) {
-                
-                return true;
-            }
-            
-        }, new Function<List<List<Integer>>,List<Integer>>() {
-
-            @Override
-            public List<Integer> apply(List<List<Integer>> t) {
-                int sum = 0;
-                for (Integer integer : t.get(0)) {
-                    sum += integer;
-                }
-                // TODO Auto-generated method stub
-                return Collections.singletonList(sum);
-            }
-            
-        });
+       return l -> l.stream().map(s -> s.stream().mapToInt(Integer::intValue).sum()).collect(Collectors.toList());
     }
 
     @Override
     public <X> Flattener<X, X> flattenAll() {
-        return generalFlatten(new Predicate<List<List<X>>>() {
-
-            @Override
-            public boolean test(List<List<X>> t) {
-                
-                return true;
-            }
-            
-        }, new Function<List<List<X>>,List<X>>() {
-
-            @Override
-            public List<X> apply(List<List<X>> t) {
-                return new ArrayList<>(t.get(0));
-            }
-            
-        });
+        return li-> li.stream().flatMap(List::stream).collect(Collectors.toList());
     }
 
     @Override
     public Flattener<String, String> concatPairs() {
-        return generalFlatten(new Predicate<List<List<String>>>() {
-
-            @Override
-            public boolean test(List<List<String>> t) {
-                return t.size() % 2 == 0;
-            }
-            
-        }, new Function<List<List<String>>,List<String>>() {
-
-            @Override
-            public List<String> apply(List<List<String>> t) {
-                String b = "";
-                for (List<String> list : t) {
-                    for (String s : list) {
-                        b = b.concat(s);
-                    }
-                }
-                return List.of(b);
-            }
-            
-        });
+        return li -> IntStream.range(
+            0, 
+            (li.size() + li.size() % 2) / 2
+        )
+        .mapToObj(
+            i -> li.stream()
+                .skip(i * 2).limit(2)
+                .map(g -> g.stream().collect(Collectors.joining()))
+                .collect(Collectors.joining())
+        )           
+        .collect(Collectors.toList());
     }
 
     @Override
     public <I, O> Flattener<I, O> each(Function<List<I>, O> mapper) {
-        return generalFlatten(new Predicate<List<List<I>>>() {
-
-            @Override
-            public boolean test(List<List<I>> t) {
-                return true;
-            }
-            
-        }, new Function<List<List<I>>,List<O>>() {
-
-            @Override
-            public List<O> apply(List<List<I>> t) {
-                List<O> c = new ArrayList<>();
-                for (List<I> list : t) {
-                    c.add(mapper.apply(list));
-                }
-                return c;
-            }
-            
-        });
+        return li -> li.stream().map(mapper).collect(Collectors.toList());
     }
 
     @Override
     public Flattener<Integer, Integer> sumVectors() {
-        return generalFlatten(new Predicate<List<List<Integer>>>() {
-
-            @Override
-            public boolean test(List<List<Integer>> t) {
-                
-                return false;
-            }
-            
-        }, new Function<List<List<Integer>>,List<Integer>>() {
-
-            @Override
-            public List<Integer> apply(List<List<Integer>> t) {
-                Map<Integer, List<Integer>> map = new HashMap<>();
-                for (List<Integer> list : t) {
-                    for (int i = 0; i < list.size(); i++) {
-                        if(map.get(i) == null){
-                            map.put(i, new ArrayList<>());
-                        }
-                       
-                        map.get(i).add(list.get(i));
-                       
-                    }
-                }
-                List<Integer> outer = new ArrayList<>();
-                for (List<Integer> lista : map.values()) {
-                    outer.add(lista.stream().mapToInt(Integer::intValue).sum());
-                }
-
-                return outer;
-            }
-            
-        });
+        return zi -> IntStream.range(0, zi.get(0).size()).mapToObj(i -> zi.stream().mapToInt(h -> h.get(i)).sum()).collect(Collectors.toList());
     }
     
 }
