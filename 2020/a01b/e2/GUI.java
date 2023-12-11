66f31fe1a5d643c4590456af1276386bbce99dd2
Keegan Carlo Falcao - keegancarlo.falcao@studio.unibo.it <keegancarlo.falcao@studio.unibo.it>
2023-12-11 21:39:58 +0100

    cleaned a04

--- 2020/a01b/e2/GUI.java
+++ 2020/a01b/e2/GUI.java
@@ -9,9 +9,11 @@ import java.awt.event.ActionListener;
 public class GUI extends JFrame {
     
     private static final long serialVersionUID = -6218820567019985015L;
-    private final List<JButton> cells = new ArrayList<>();
-       
+    private final Map<JButton, Coord> cells = new HashMap<>();
+    private final Logic logic;
+
     public GUI(int size) {
+        logic = new LogicImpl((size));
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setSize(100*size, 100*size);
         
@@ -20,19 +22,32 @@ public class GUI extends JFrame {
         
         ActionListener al = e -> {
         	var button = (JButton)e.getSource();
-        	var position = cells.indexOf(button);
+        	var position = cells.get(button);
         	button.setText(""+position);
         };
                 
         for (int i=0; i<size; i++){
             for (int j=0; j<size; j++){
                 final JButton jb = new JButton(" ");
-                this.cells.add(jb);
+                this.cells.put(jb, new Coord(j, i));
                 jb.addActionListener(al);
                 panel.add(jb);
             }
         }
+        draw();
         this.setVisible(true);
+    }
+
+
+    private void draw() {
+        cells.forEach((b, c) -> {
+            if(logic.getMap().containsKey(c)) {
+                b.setText(logic.getMap().get(c).getContent());
+            }
+            else {
+                b.setText("");
+            }
+        });
     }
     
 }
