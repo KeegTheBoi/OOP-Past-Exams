f02acd4fcd5ae74ee2c6aef238600039f9844e7f
Keegan Carlo Falcao - keegancarlo.falcao@studio.unibo.it <keegancarlo.falcao@studio.unibo.it>
2023-12-15 14:59:36 +0100

    to revert

--- 2021/a05/e1/StateFactoryImpl.java
+++ 2021/a05/e1/StateFactoryImpl.java
@@ -2,18 +2,13 @@ package a05.e1;
 
 import java.util.Iterator;
 import java.util.function.Function;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class StateFactoryImpl implements StateFactory {
-
-    
-
 
     @Override
     public <S, A> State<S, A> fromFunction(Function<S, Pair<S, A>> fun) {
         return new State<S,A>() {
-
+            A current;
             @Override
             public S nextState(S s) {
                 return fun.apply(s).get1();
@@ -21,19 +16,19 @@ public class StateFactoryImpl implements StateFactory {
 
             @Override
             public A value(S s) {
-                return fun.apply(s).get2();
+                current = fun.apply(s).get2();
+                return current;
             }
 
             @Override
-            public <B> State<S, B> map(Function<A, B> sfun) {
-                return fromFunction(k -> new Pair<S, B>(nextState(k), sfun.apply(value(k))));
+            public <B> State<S, B> map(Function<A, B> func) {
+                return fromFunction(s -> new Pair<>(fun.apply(s).get1(), func.apply(fun.apply(s).get2())));
             }
 
             @Override
             public Iterator<A> iterator(S s0) {
-                var map = Stream.iterate(new Pair<S, A>(s0, value(s0)), o -> new Pair<S, A>(nextState(o.get1()), value(nextState(o.get1())))).limit(4)
-                .collect(Collectors.toMap(s -> s.get1(), v -> v.get2()));
-                return map.values().iterator();
+                // TODO Auto-generated method stub
+                return null;
             }
             
         };
@@ -54,14 +49,15 @@ public class StateFactoryImpl implements StateFactory {
             }
 
             @Override
-            public <K> State<S, K> map(Function<B, K> fun) {             
-                return fromFunction(k -> new Pair<S, K>(nextState(k), fun.apply(value(k))));
+            public <L> State<S, L> map(Function<B, L> fun) {
+                
+                return fromFunction(s -> new Pair<S, L>(nextState(s) , fun.apply(value(s))));
             }
 
             @Override
             public Iterator<B> iterator(S s0) {
-                var pairIterator = Stream.iterate(new Pair<>(s0, value(s0)), o -> new Pair<>(nextState(o.get1()), value(nextState(o.get1())))).iterator();
-                return Stream.generate(() -> null).takeWhile(x -> pairIterator.hasNext()).map(n -> pairIterator.next().get2()).iterator();
+                // TODO Auto-generated method stub
+                return null;
             }
             
         };
@@ -69,24 +65,14 @@ public class StateFactoryImpl implements StateFactory {
 
     @Override
     public State<Integer, String> incSquareHalve() {
-        State<Integer, String> first = fromFunction(u -> new Pair<Integer, String>(u + 1, String.valueOf(u + 1)));
-        State<Integer, String> second = fromFunction(u -> new Pair<Integer, String>(u * u, String.valueOf(u * u)));
-        State<Integer, String> third = fromFunction(u -> new Pair<Integer, String>(u / 2, String.valueOf(u / 2)));
-        return compose(compose(first, second), third);
+        // TODO Auto-generated method stub
+        return null;
     }
 
     @Override
     public State<Integer, Integer> counterOp(CounterOp op) {
-        switch(op) {
-            case INC:
-                return fromFunction(u -> new Pair<Integer, Integer>(u + 1, null));
-            case RESET:
-                return fromFunction(u -> new Pair<Integer, Integer>(0, null));
-            case GET:
-                return fromFunction(u -> new Pair<Integer, Integer>(u, u));
-            default:
-                return null;
-        }
+        // TODO Auto-generated method stub
+        return null;
     }
-    
+
 }
