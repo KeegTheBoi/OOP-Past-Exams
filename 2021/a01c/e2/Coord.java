518043dceceb49fd1abf4b96e2481f6a6d1ee662
Keegan Carlo Falcao - keegancarlo.falcao@studio.unibo.it <keegancarlo.falcao@studio.unibo.it>
2023-12-18 12:00:29 +0100

    bro stai zitto, frega una minkia della tua storia, btw a01c done

---
+++ 2021/a01c/e2/Coord.java
@@ -0,0 +1,5 @@
+package a01c.e2;
+
+public record Coord (int x, int y){
+
+}
--- 2021/a01c/e2/GUI.java
+++ 2021/a01c/e2/GUI.java
@@ -2,16 +2,17 @@ package a01c.e2;
 
 import javax.swing.*;
 import java.util.*;
-import java.util.List;
 import java.awt.*;
 import java.awt.event.ActionListener;
 
 public class GUI extends JFrame {
     
     private static final long serialVersionUID = -6218820567019985015L;
-    private final List<JButton> cells = new ArrayList<>();
+    private final Map<JButton, Coord> cells = new HashMap<>();
+    private final Logic log;
     
     public GUI(int size) {
+        log = new LogicImpl();
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setSize(50*size, 50*size);
         
@@ -20,19 +21,27 @@ public class GUI extends JFrame {
         
         ActionListener al = e -> {
         	var button = (JButton)e.getSource();
-        	button.setText(""+cells.indexOf(button));
-        	button.setEnabled(false); 
+            log.hit(cells.get(button));
+            draw();
         };
                 
         for (int i=0; i<size; i++){
             for (int j=0; j<size; j++){
                 final JButton jb = new JButton(" ");
-                this.cells.add(jb);
+                this.cells.put(jb, new Coord(j, i));
                 jb.addActionListener(al);
                 panel.add(jb);
             }
         }
         this.setVisible(true);
+    }
+
+    private void draw() {
+        cells.forEach((k, v) -> {
+            if(log.getSet().contains(v)) {
+                k.setText("*");
+            }
+        });
     }
     
 }
