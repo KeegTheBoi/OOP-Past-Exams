2ce9fa248d9a2a86a2dbff5f580f9ca0d7ebcb33
Keegan Carlo Falcao <keeganfalcao@gmail.com>
2023-12-26 08:37:15 +0100

    Update ScannerFactoryImpl.java

--- 2020/a02a/e1/ScannerFactoryImpl.java
+++ 2020/a02a/e1/ScannerFactoryImpl.java
@@ -15,64 +15,29 @@ public class ScannerFactoryImpl implements ScannerFactory {
 
     @Override
     public <X, Y> Scanner<X, List<Y>> collect(Predicate<X> filter, Function<X, Y> mapper) {
-        return new Scanner<X,List<Y>>() {
-
-            @Override
-            public List<Y> scan(Iterator<X> input) {
-                return toStream(input).filter(filter).map(mapper).collect(Collectors.toList());
-            }
-            
-        };
+        return iter -> toStream(iter).filter(filter).map(mapper).collect(Collectors.toList());
     }
 
     private <X> Stream<X> toStream(Iterator<X> input) {
-        return input.hasNext() ? Stream.iterate(input.next(), c -> c != null, l -> input.hasNext() ? input.next() : null) : Stream.empty();
+        return Stream.iterate(input, c -> c.hasNext(), UnaryOperator.identity()).map(Iterator::next);
     }
 
     @Override
     public <X> Scanner<X, Optional<X>> findFirst(Predicate<X> filter) {
-        return new Scanner<X,Optional<X>>() {
-
-            @Override
-            public Optional<X> scan(Iterator<X> input) {
-                return toStream(input).filter(filter).findFirst();
-            }
-        };
+        return input -> toStream(input).filter(filter).findFirst();
     }
 
     @Override
     public Scanner<Integer, Optional<Integer>> maximalPrefix() {
-        return new Scanner<Integer,Optional<Integer>>() {
-
-            @Override
-            public Optional<Integer> scan(Iterator<Integer> input) {            
-                var prefixVals = toStream(input).collect(Collectors.toList());
-                if(prefixVals.isEmpty()) {
-                    return Optional.empty();
-                }
-                Integer max = prefixVals.get(prefixVals.size()-1);
-                for (int i = 0; i < prefixVals.size() - 1; i++) {
-                    if(prefixVals.get(i) > prefixVals.get(i + 1)) {
-                        max = prefixVals.get(i);
-                        break;
-                    }
-                }
-                final Integer maxFinal = max;
-                return prefixVals.stream().filter(c -> c == maxFinal).findFirst();
-            }            
-        };
+        return input -> {
+            var list = toStream(input).collect(Collectors.toList());
+            IntStream.range(0, list.size()).dropWhile(i-> list.toStream().limit(i).allMatch(y-> y <= list.get(i))).map(List::get).findFirst();
+        }
     }
 
     @Override
     public Scanner<Integer, List<Integer>> cumulativeSum() {
-        return new Scanner<Integer,List<Integer>>() {
-
-            @Override
-            public List<Integer> scan(Iterator<Integer> input) {
-                return toStream(input).collect(ArrayList<Integer>::new, (m, l) -> m.add(m.isEmpty() ? l : m.get(m.size() - 1) + l), ArrayList::addAll);             
-            }
-            
-        };
+        return input -> toStream(input).collect(ArrayList<Integer>::new, (m, n) -> m.add(m.isEmpty() ? n : m.get(m.size() - 1) + n), ArrayList::addAll);             
     }
 
 }
