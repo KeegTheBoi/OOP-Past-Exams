64cf3aad50d2b140b663300bc17daa053becf9a1
Keegan Carlo Falcao <keeganfalcao@gmail.com>
2023-12-31 09:20:44 +0100

    Update ListSplitterFactoryImpl.java

--- 2020/a02c/e1/ListSplitterFactoryImpl.java
+++ 2020/a02c/e1/ListSplitterFactoryImpl.java
@@ -9,17 +9,18 @@ import java.util.stream.IntStream;
 
 public class ListSplitterFactoryImpl implements ListSplitterFactory {
 
-    private static class Splitter<X> implements ListSplitter<X>{
+    private static class Splitter<X> implements ListSplitter<X> {
 
         private int delta;
-        private Predicate<Integer> pred;
+        private BiPredicate<List<X>, Integer> bipred;
 
-        public Splitter(final Predicate<Integer> pred) {
-            this.pred = pred;
+        public Splitter(final BiPredicate<List<X>, Integer> bipred) {
+            this.bipred = bipred;
         }
         
         @Override
         public List<List<X>> split (List<X> list) {
+            Predicate<Intger> pred = i -> bipred.test(list, i);
             return Stream.iterate(0, d -> d < list.size(), i -> i + delta)
                 .map (k -> 
                     Optional.of(sequence(k, list, pred))
@@ -33,22 +34,30 @@ public class ListSplitterFactoryImpl implements ListSplitterFactory {
         private List<X> sequence(final int offset, final List<X> list) {
             return IntStream.range(0, list.size()).boxed().skip(offset).takeWhile(pred).map(list::get).collect(Collectors.toList());
         }
+
+        private static ListSplitter<X> splitFixed(final int dividend) {
+            return list -> IntStream.range(0, list.size() / dividend)
+                .mapToObj(y -> list.stream().skip(dividend * y).limit(dividend).collect(Collectors.toList()))
+                .collect(Collectors.toList());
+        }
+        
+    
     }
 
 
     @Override
     public <X> ListSplitter<X> asPairs() {
-        return new Splitter<>(i -> i / 2 % 2 == 0);
+        return Splitter.splitFixed(2);
     }
 
     @Override
     public <X> ListSplitter<X> asTriplets() {
-        return new Splitter<>(i -> i / 3 % 2 == 0);
+        return Splitter.splitFixed(3);
     }
 
     @Override
     public <X> ListSplitter<X> asTripletsWithRest() {
-        return new Splitter<>(i -> i / 3 % 2 == 0);
+        return new Splitter<>((l, i) -> i / 3 % 2 == 0);
     }
 
     @Override
@@ -58,7 +67,7 @@ public class ListSplitterFactoryImpl implements ListSplitterFactory {
 
     @Override
     public <X> ListSplitter<X> byPredicate(Predicate<X> predicate) {
-        return new Splitter<>(i -> predicate.test(l.get(i));
+        return new Splitter<>((l, i)-> predicate.test(l.get(i)));
     }
     
 }
