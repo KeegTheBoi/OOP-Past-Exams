d5440fb0251623465ef4c1124034e73406dbf14a
Keegan Carlo Falcao <keegnacarlo.falcao@studio.unibo.it>
2024-01-10 09:51:08 +0100

    guii

--- 2019/a03a/e2/GUI.java
+++ 2019/a03a/e2/GUI.java
@@ -1,15 +1,55 @@
 package a03a.e2;
 
 import javax.swing.*;
-import java.util.*;
+import java.awt.event.*;
 import java.awt.*;
-import java.awt.event.ActionListener;
+import java.util.*;
+import java.util.List;
 
 public class GUI extends JFrame {
     
+    private Map<JButton, Coord> cells = new HashMap<>();
+    private final Logic log;
     
-    public GUI(int size) {
+    public GUI(final int size) {
+		log = new LogicImpl(size);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setSize(500, 500);
+        int cols = size; // {1,2,3,4,5}
+        JPanel panel = new JPanel(new GridLayout(cols,cols));
+        this.getContentPane().add(BorderLayout.CENTER,panel);
+        final JButton mv = new JButton(">");
+        this.getContentPane().add(BorderLayout.SOUTH, mv);
+        
+        ActionListener al = (e)->{
+            final JButton bt = (JButton)e.getSource();
+            var pos = cells.get(bt);
+            log.hit(pos);
+			draw();
+        };
+        
+        mv.addActionListener( e -> {
+			log.move();
+			if(log.isOver()) {
+				System.exit(0);
+			}
+			draw();
+		});
+		
+        for (int i=0;i<size;i++){
+			for (int j=0;j<size;j++){
+				final JButton jb = new JButton();
+				jb.addActionListener(al);
+				this.cells.put(jb, new Coord(j, i));
+				panel.add(jb);
+            }
+        } 
+        draw();
+        this.setVisible(true);
     }
-    
-    
+
+        
+    private void draw() {
+		this.cells.forEach((k, v) -> k.setText(log.getMap().containsKey(v) ? log.getMap().get(v).getSymbol() : ""));
+	}
 }
---
+++ 2019/a03a/e2/LogicImpl.java
@@ -0,0 +1,77 @@
+package a03a.e2;
+
+import java.util.*;
+import java.util.stream.*;
+import java.util.function.*;
+
+public class LogicImpl implements Logic {
+	private enum Direction {
+		UP(c -> new Coord(c.x(), c.y() -1)), 
+		RIGHT(c -> new Coord(c.x() + 1, c.y())),
+		DOWN(c -> new Coord(c.x(), c.y() + 1)),
+		LEFT(c -> new Coord(c.x() - 1, c.y()));
+		
+		private final UnaryOperator<Coord> oper;
+		
+		private Direction(final UnaryOperator<Coord> oper) {
+			this.oper = oper;
+		}
+		
+		public Coord move(Coord c) {
+			return this.oper.apply(c);
+		}
+		
+		public Direction next() {
+			return Direction.values()[(this.ordinal() + 1) % values().length];
+		}
+	}
+	private final int size;
+	private final Map<Coord, Type> map;
+	private Direction dire = Direction.UP;
+	private Coord moving;
+	
+	public LogicImpl(final int size) {
+		this.size = size;
+		this.map = new HashMap<>();
+		init();
+	}
+	
+	private void init() {
+		this.moving = new Coord(0, 0);
+		map.put(this.moving, Type.Moving);
+	}
+	
+	private boolean isLimit(Coord c) {
+		return (c.x() == 0 && c.y() == 0) || (c.x() == size - 1 && c.y() == size - 1)
+			|| (c.x() == 0 && c.y() == size - 1) || (c.x() == size - 1  && c.y() == 0);
+	}
+	
+	public void hit(Coord c) {
+		this.map.put(c, Type.Stationary);
+	}
+	
+	public void move() {
+		this.map.remove(this.moving);
+		this.moving = getNext();
+		Optional.of(map).filter(g -> !this.isOver()).ifPresent(m -> m.put(moving, Type.Moving));
+	}
+	
+	private Coord getNext() {
+		return Optional.of(this.moving).filter(this::isLimit).map(peek(x -> dire = dire.next())).map(dire::move).orElse(dire.move(this.moving));
+	}
+	
+	private <T> UnaryOperator<T> peek(Consumer<T> action) {
+		return x -> {
+			action.accept(x);
+			return x;
+		};
+	}
+	
+	public boolean isOver() {
+		return this.map.entrySet().stream().filter(e -> e.getValue().equals(Type.Stationary)).anyMatch(k -> k.getKey().equals(this.moving));
+	}
+	
+	public Map<Coord, Type> getMap() {
+		return this.map;
+	}
+}
